fee41561298e47d1e3318e5e1c9d280f4d6d8e14
cold-thunder <mdhemal202000@gmail.com>
2024-08-10 16:22:08 +0600

    updated

--- lib/widgets/list_tile.dart
+++ lib/widgets/list_tile.dart
@@ -21,50 +21,57 @@ class ListTileWidget extends StatelessWidget{
       onTap:(){
         tapFunc(todo);
       },
-      child: ListTile(
-        tileColor: Colors.blueGrey,
-        leading: CircleAvatar(
-          radius: 20,
-          backgroundColor: Colors.purple,
-          child: Text('${index+1}',
-              style: TextStyle(fontSize: 22, color: Colors.white)
+      child: Container(
+        margin: EdgeInsets.all(5),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: Colors.blueGrey,
+        ),
+        child: ListTile(
+            tileColor: Colors.blueGrey,
+            leading: CircleAvatar(
+                radius: 20,
+                backgroundColor: Colors.purple,
+                child: Text('${index+1}',
+                    style: TextStyle(fontSize: 22, color: Colors.white)
+                )
+            ),
+            title: Text('${todo['title']}',
+              overflow: TextOverflow.fade,
+              maxLines: 1,
+            ),
+            titleTextStyle: TextStyle(fontSize: 24, color: Colors.white),
+            subtitle: Text('${todo['description']}',
+                overflow: TextOverflow.fade,
+                maxLines: 1
+            ),
+            subtitleTextStyle: TextStyle(
+                fontSize: 16,
+                color: Colors.white
+            ),
+            trailing: PopupMenuButton(
+                onSelected: (value){
+                  if(value == 'edit'){
+                    editData(todo);
+                  }else if(value == 'delete'){
+                    deleteById(todo['_id'].toString());
+                  }
+                },
+                itemBuilder: (context){
+                  return [
+                    PopupMenuItem(
+                        child: Text('Edit', style: TextStyle(fontSize: 20)),
+                        value: 'edit'
+                    ),
+                    PopupMenuItem(
+                        child: Text('Delete', style: TextStyle(fontSize: 20)),
+                        value: 'delete'
+                    )
+                  ];
+                }
             )
-          ),
-          title: Text('${todo['title']}',
-            overflow: TextOverflow.fade,
-            maxLines: 1,
-          ),
-          titleTextStyle: TextStyle(fontSize: 24, color: Colors.white),
-          subtitle: Text('${todo['description']}',
-            overflow: TextOverflow.fade,
-            maxLines: 1
-          ),
-          subtitleTextStyle: TextStyle(
-            fontSize: 16,
-            color: Colors.white
-          ),
-          trailing: PopupMenuButton(
-          onSelected: (value){
-            if(value == 'edit'){
-              editData(todo);
-            }else if(value == 'delete'){
-              deleteById(todo['_id'].toString());
-            }
-            },
-          itemBuilder: (context){
-            return [
-              PopupMenuItem(
-                child: Text('Edit', style: TextStyle(fontSize: 20)),
-                  value: 'edit'
-              ),
-              PopupMenuItem(
-                child: Text('Delete', style: TextStyle(fontSize: 20)),
-                  value: 'delete'
-                 )
-              ];
-            }
-          )
-          )
+        )
+      )
     );
   }
 }
